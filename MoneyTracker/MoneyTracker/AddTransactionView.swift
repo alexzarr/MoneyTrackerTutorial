e58b8cad6de8f0de6f68b0728c5d7897b0b69bb0
Alex <31429269+alexzarr@users.noreply.github.com>
2023-08-14 17:30:13 -0600

    Dismissing AddTransactionView

--- MoneyTracker/MoneyTracker/AddTransactionView.swift
+++ MoneyTracker/MoneyTracker/AddTransactionView.swift
@@ -8,30 +8,48 @@
 import SwiftUI
 
 struct AddTransactionView: View {
+    @Environment(\.presentationMode) private var presentationMode
+    
     @StateObject var viewModel = AddTransactionViewModel()
     
     var body: some View {
-        VStack(spacing: 20) {
-            Picker("", selection: $viewModel.type) {
-                ForEach(MTTransactionType.allCases) { type in
-                    Text(type.title)
+        NavigationView {
+            VStack(spacing: 20) {
+                Picker("", selection: $viewModel.type) {
+                    ForEach(MTTransactionType.allCases) { type in
+                        Text(type.title)
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                TextField("$0.00", value: $viewModel.amount, format: .currency(code: viewModel.currencyCode))
+                    .multilineTextAlignment(.trailing)
+                    .lineLimit(1)
+                    .font(.title2)
+                    .fontWeight(.thin)
+                    .textFieldStyle(.roundedBorder)
+                    .keyboardType(.decimalPad)
+                
+                DatePicker(selection: $viewModel.madeOn, in: ...Date.now, displayedComponents: .date) {
+                    Text("Made on:")
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle(Text("New transaction"))
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button {
+                        presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("Cancel")
+                    }
                 }
             }
-            .pickerStyle(.segmented)
-            
-            TextField("$0.00", value: $viewModel.amount, format: .currency(code: viewModel.currencyCode))
-                .multilineTextAlignment(.trailing)
-                .lineLimit(1)
-                .font(.title2)
-                .fontWeight(.thin)
-                .textFieldStyle(.roundedBorder)
-                .keyboardType(.decimalPad)
-            
-            DatePicker(selection: $viewModel.madeOn, in: ...Date.now, displayedComponents: .date) {
-                Text("Made on:")
-            }
+            .interactiveDismissDisabled()
         }
-        .padding(.horizontal)
     }
 }
 
